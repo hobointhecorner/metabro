892acf7e012d98670fd51f94ef1d9a3ff89f6fb5
Cody Lape <cody@hobomedia.net>
2017-09-17 15:16:41 -0500

    Created MB.Install project

---
+++ MB.Install/Install-Metabro.ps1
@@ -0,0 +1,105 @@
+﻿[cmdletbinding()]
+param(
+    [Parameter(ValueFromPipeline = $true)]
+    [string[]]$Module = "MB.*",
+    [string]$Source = '\\file01\scripts\metabro\release',
+    [string]$Destination = 'c:\program files\windowspowershell\modules',
+    [switch]$Force = $true
+)
+
+begin
+{
+    $LogName = "Metabro"
+    $oldInfoPref = $InformationPreference
+    $InformationPreference = 'continue'
+
+    If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+    {   
+        Write-Error "Installation must be run as admin to access the Program Files directory and create event log sources."
+        $InformationPreference = $oldInfoPref
+        break
+    }
+
+    $logExists = [System.Diagnostics.EventLog]::Exists($LogName)
+
+    function Install-MBModule
+    {
+        [cmdletbinding()]
+        param(
+            [string]$SourcePath,
+            [string]$DestinationPath,
+            [string]$LogName,
+            [switch]$Force
+        )
+
+        begin
+        {
+            $moduleName = Split-Path $SourcePath -Leaf
+            $modulePath = Join-Path $DestinationPath $moduleName
+            $newLine = $false
+        }
+
+        process
+        {
+            #Copy module
+            if (!(Test-Path $modulePath) -or $Force)
+            {
+                Write-Information "Copying module $($_.Name) to $Destination"
+                Copy-Item -LiteralPath $SourcePath -Destination $DestinationPath -Force:$Force -Recurse
+                $newLine = $true
+            }
+
+            #Create event source
+            $logSource = $moduleName
+            $sourceExists = [System.Diagnostics.EventLog]::SourceExists($logSource)
+            if ($Force -and $sourceExists)
+            {
+                Write-Information "Removing log source $logSource"
+                Remove-EventLog -Source $logSource
+                $sourceExists = $false
+            }
+
+            if (!$sourceExists)
+            {
+                Write-Information "Creating log source $logSource"
+                New-EventLog -LogName $LogName -Source $logSource
+                $newLine = $true
+            }
+
+            #Run init script, if one exists
+            if ($initScript = gci $modulePath -Filter "Initialize-*.ps1" | select -First 1)
+            {
+                #Write-Information "`n"
+                . $initScript.FullName -Force:$Force
+            }
+        }
+
+        end
+        {
+            if ($newLine) { Write-Information "`n" }
+        }
+    }
+}
+
+process
+{
+    if (($Module -contains 'MB.Pref') -or ($Module -ieq 'mb.*'))
+    {
+        gci $Source -Filter 'MB.Pref' |
+            foreach { Install-MBModule -SourcePath $_.FullName -DestinationPath $Destination -LogName $LogName -Force:$Force }
+    }
+
+    foreach ($moduleName in $Module)
+    {
+        gci $Source -Filter $moduleName |
+            where { $_.Name -ine 'mb.pref' } |
+            foreach {
+                Install-MBModule -SourcePath $_.FullName -DestinationPath $Destination -LogName $LogName -Force:$Force
+            }
+    }
+}
+
+end
+{
+    $InformationPreference = $oldInfoPref
+}
